3732e65aa7702715ea7b959f435fdd74df3170c3
zrks <marcis.veveris@outlook.com>
2025-03-15 08:28:33 +0200

    Cleanup ?

---
+++ do-infra/variables.tf
@@ -0,0 +1,9 @@
+variable "digitalocean_token" {
+  description = "DigitalOcean API Token"
+  type        = string
+}
+
+variable "ssh_key" {
+  description = "Your SSH key ID from DigitalOcean"
+  type        = string
+}
